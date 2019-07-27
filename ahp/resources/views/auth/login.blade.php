c60b94d90fd030cdf1d6ca7784c03870f5ec821e
Dewi Anisa Istiaomah <dewianisaist@gmail.com>
2019-07-27 22:15:59 +0700

    edit login view

--- ahp/resources/views/auth/login.blade.php
+++ ahp/resources/views/auth/login.blade.php
@@ -24,7 +24,6 @@
                                     <input id="identity_number" type="text" class="form-control" name="identity_number" placeholder="Nomor Identitas" required value="{{ old('identity_number') }}">
                                 </div>
                             </div>
-
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                                 <div>
                                     @if ($errors->has('password'))
@@ -38,17 +37,6 @@
                                     <input id="password" type="password" class="form-control" name="password" placeholder="Password" required>
                                 </div>
                             </div>
-
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="checkbox" name="remember"> Ingat Saya
-                                        </label>
-                                    </div>
-                                </div>
-                            </div>
-
                             <div class="row">
                                 <div class="col-6">
                                     <button type="submit" class="btn btn-primary px-2"><i class="fa fa-btn fa-sign-in"></i> Login</button>
